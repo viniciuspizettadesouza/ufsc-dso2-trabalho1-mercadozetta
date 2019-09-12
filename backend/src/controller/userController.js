e972215c3e9d4789d27f92f9f01be18984ff844b
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-12 12:50:33 -0300

    Configuradas funções de registro e login de user

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,24 +1,45 @@
 const User = require('../model/user');
 
 module.exports = {
-    async store(req, res) {
-        const { username } = req.body;
+    login(req, res) {
+        let { email, password } = req.body;
 
-        const userExists = await User.findOne({ user: username });
+        User.findOne({ email: email, password: password }, function (err, email) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send();
+            }
+            if (!email) {
+                return res.status(404).send();
+            }
+            console.log("Logado com sucesso")
+            return res.status(200).send();
+        })
+    },
+
+    async register(req, res) {
+        let { email, password, firstname, telephone } = req.body;
+
+        const userExists = await User.findOne({ user: email });
 
         if (userExists) {
             return res.json(userExists)
         }
 
-        const user = await User.create({
-            email,
-            password,
-            name,
-            user,
-            telephone,
-            avatar
+        const newUser = await User.create({
+            email: email,
+            password: password,
+            firstname: firstname,
+            telephone: telephone,
         })
 
-        return res.json({ user });
-    }
+        newUser.save(function (err, savedUser) {
+            if (err) {
+                console.log(err);
+                return res.status(500).send();
+            }
+            console.log("Cadastrado com sucesso")
+            return res.status(200).send();
+        })
+    },
 };
