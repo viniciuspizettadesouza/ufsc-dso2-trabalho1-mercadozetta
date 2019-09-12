5b95b29a36b23fe0e605983510870949e8bfd181
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-12 09:37:19 -0300

    config de rotas via userController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,13 +1,12 @@
 const express = require('express');
+const userController = require('./controller/userController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
-    return res.json({ message: `Hello ${req.query.name}` })
+    return res.json({ message: `Hello ${req.query.name}` });
 })
 
-routes.post('/users', (req, res) => {
-    return res.json({ ok: true })
-})
+routes.post('/users', userController.store);
 
-module.exports = routes
+module.exports = routes;
