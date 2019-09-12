efe7f2a320456d13626333c48a3dea0f6e536102
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-12 09:37:46 -0300

    dependencia mongoose e conexao com mongobd

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const routes = require('./routes');
 
 const server = express();
 
+mongoose.connect('mongodb+srv://zetta:zetta@clusterzetta-mcozz.mongodb.net/trabalho1?retryWrites=true&w=majority', {
+    useUnifiedTopology: true, useNewUrlParser: true
+});
+server.use(express.json());
 server.use(routes);
 
 server.listen(3333);
