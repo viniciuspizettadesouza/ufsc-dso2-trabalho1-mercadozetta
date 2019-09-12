2ba0003b84c7d03fce3acbaa92383aed26f4a892
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-12 17:54:43 -0300

    Adicinadas rotas get para listar produtos e post para adicionar produtos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const UserController = require('./controller/UserController');
+const ProductController = require('./controller/ProductController');
 
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     return res.json({ message: `Hello ${req.query.name}` });
 })
+routes.get('/products', ProductController.index);
 
 routes.post('/login', UserController.login);
 
 routes.post('/register', UserController.register);
+
+routes.post('/user/:userID/addproduct', ProductController.add);
 
 module.exports = routes;
