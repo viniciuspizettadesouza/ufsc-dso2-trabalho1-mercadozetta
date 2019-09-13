7813ad84ab37b4e319abaa5d64a92528847dbd0a
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-13 16:16:10 -0300

    Hash no password, email lowercase
    utilizado bcryptjs

--- backend/src/model/user.js
+++ backend/src/model/user.js
@@ -1,22 +1,37 @@
 const { Schema, model } = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     email: {
         type: String,
         required: true,
+        lowercase: true,
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        select: false,
     },
-    firstname: String,
-    telephone: String,
+    firstname: {
+        type: String,
+        lowercase: true,
+    },
+    telephone: {
+        type: String,
+        lowercase: true,
+    },
     products: [{
         type: String,
         ref: 'User',
     }]
 }, {
     timestamps: true,
+});
+
+UserSchema.pre('save', async function (next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
 });
 
 module.exports = model('user', UserSchema);
