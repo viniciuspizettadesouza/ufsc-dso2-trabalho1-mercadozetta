39476ac91d3c4439be7a794de6154083ef26c248
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-13 16:16:49 -0300

    Autenticação será feita em arquivo próprio

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -15,18 +15,4 @@ module.exports = {
             return res.status(200).send(req.body.email);
         })
     },
-
-    async register(req, res) {
-        let { email, password, firstname, telephone } = req.body;
-
-        try {
-            if (await User.findOne({ email }))
-                return res.status(400).send({ error: 'User already exists' });
-            const newUser = await User.create(req.body);
-            newUser.password = undefined;
-            return res.status(200).send({ newUser });
-        } catch (err) {
-            return res.status(400).send({ error: 'Registration failed' });
-        }
-    },
 };
