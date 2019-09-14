b73af50f09c17277b65bb1ccadadd15258ddf801
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-14 03:45:18 -0300

    Adicionado cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://zetta:zetta@clusterzetta-mcozz.mongodb.net/merca
     useUnifiedTopology: true, useNewUrlParser: true
 });
 
+server.use(cors());
 server.use(express.json());
 server.use(routes);
 
