086500d6ac235a8215f3bb8a6cd74563d07eec00
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 11:39:30 -0300

    new function authenticate

--- backend/src/controller/AuthController.js
+++ backend/src/controller/AuthController.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const User = require('../model/User');
 
 module.exports = {
@@ -13,5 +14,18 @@ module.exports = {
         } catch (err) {
             return res.status(400).send({ error: 'Registration failed' });
         }
+    },
+
+    async authenticate(req, res) {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email }).select('password');
+        if (!user)
+            return res.status(400).send({ error: 'User not found' })
+
+        if (!await bcrypt.compare(password, user.password))
+            return res.status(400).send({ error: 'Invalid password' })
+
+        user.password = undefined;
+        res.send({ user });
     },
 };
