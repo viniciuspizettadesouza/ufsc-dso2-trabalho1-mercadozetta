d3471421d5cc11474366dabb86ed4e76bfe61ea2
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 18:06:37 -0300

    new object product

---
+++ backend/src/model/Product.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const ProductSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        lowercase: true,
+    },
+    description: {
+        type: String,
+    },
+    quant: {
+        type: String,
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+module.exports = model('product', ProductSchema);
