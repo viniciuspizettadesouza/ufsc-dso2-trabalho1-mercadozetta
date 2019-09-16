ab2964b459a50e411311235acd7c19994687db82
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 11:40:09 -0300

    updated main to show products

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -1,44 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Main.css';
+
+import api from '../services/api';
 
 import logo from '../assets/logo.svg'
 
 export default function Main({ match }) {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        async function loadProducts() {
+            const response = await api.get('/register', {
+                headers: {
+                    product: match.params.id,
+                }
+            })
+            setProducts(response.data);
+        }
+        loadProducts();
+    }, [match.params.id]);
+
     return (
         <div className="main-container">
+            <Link to="/">
+                <img src={logo} alt="Tindev" />
+            </Link>
             <img src={logo} alt="logo" />
             <ul>
-                <li>
-                    <img src="https://www.gsuplementos.com.br/upload/produto/imagem/creatina-250g-creapure-growth-supplements.jpg" alt="creatina" />
-                    <footer>
-                        <strong>CREATINA (250G) (CREAPURE®) - GROWTH SUPPLEMENTS</strong>
-                        <p>GROWTH SUPPLEMENTS</p>
-                    </footer>
-                </li>
-                <li>
-                    <img src="http://images.tcdn.com.br/img/img_prod/499313/creatina_powder_200g_universal_nutrition_520_1_20170921103339.jpg" alt="creatina" />
-                    <footer>
-                        <strong>Creatina 400 g - Universal</strong>
-                        <p>É US GURI</p>
-                        <p>TO DE HORROR</p>
-                    </footer>
-                </li>
-                <li>
-                    <img src="http://images.tcdn.com.br/img/img_prod/499313/creatina_powder_200g_universal_nutrition_520_1_20170921103339.jpg" alt="creatina" />
-                    <footer>
-                        <strong>Creatina 400 g - Universal</strong>
-                        <p>É US GURI</p>
-                        <p>TO DE HORROR</p>
-                    </footer>
-                </li>
-                <li>
-                    <img src="http://images.tcdn.com.br/img/img_prod/499313/creatina_powder_200g_universal_nutrition_520_1_20170921103339.jpg" alt="creatina" />
-                    <footer>
-                        <strong>Creatina 400 g - Universal</strong>
-                        <p>É US GURI</p>
-                        <p>TO DE HORROR</p>
-                    </footer>
-                </li>
+                {products.map(user => (
+                    <li>
+                        <img src="https://www.gsuplementos.com.br/upload/produto/imagem/creatina-250g-creapure-growth-supplements.jpg" alt="creatina" />
+                        <footer>
+                            <strong>{user.product}</strong>
+                            <p>GROWTH SUPPLEMENTS</p>
+                        </footer>
+                    </li>
+                ))}
             </ul>
         </div>
 
