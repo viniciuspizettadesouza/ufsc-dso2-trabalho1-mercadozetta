117ec4cdeddd3542c891da59fd7da8883b63b213
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 11:37:15 -0300

    udpate routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+import Register from './pages/Register';
 import Login from './pages/Login';
 import Main from './pages/Main';
+import Index from './pages/Index';
+
 
 export default function Routes() {
     return (
         <BrowserRouter>
-            <Route path="/" exact component={Login} />
+            <Route path="/" exact component={Index} />
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" component={Register} />
             <Route path="/user/:id" component={Main} />
         </BrowserRouter>
     );
