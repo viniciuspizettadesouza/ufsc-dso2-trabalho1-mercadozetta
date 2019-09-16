5febaf07f2e253f2b3a54e77c5b49cee4a799604
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 10:03:42 -0300

    update login to real login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,21 +1,19 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './Register.css';
 
 import api from '../services/api';
 
 import logo from '../assets/logo.svg'
 
 export default function Login({ history }) {
-    const [username, setUsername] = useState('');
-    const [telephone, setTelephone] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     async function handleSubmit(e) {
         e.preventDefault();
 
-        const response = await api.post('/register', {
-            username, telephone, email, password
+        const response = await api.post('/login', {
+            email, password
         });
 
         const { _id } = response.data;
@@ -28,16 +26,6 @@ export default function Login({ history }) {
             <form onSubmit={handleSubmit}>
                 <img src={logo} alt="logo" />
                 <input
-                    placeholder="Nome"
-                    value={username}
-                    onChange={e => setUsername(e.target.value)}
-                />
-                <input
-                    placeholder="Telefone"
-                    value={telephone}
-                    onChange={e => setTelephone(e.target.value)}
-                />
-                <input
                     placeholder="E-mail"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
@@ -47,7 +35,7 @@ export default function Login({ history }) {
                     value={password}
                     onChange={e => setPassword(e.target.value)}
                 />
-                <button type="submit">Criar conta</button>
+                <button type="submit">Login</button>
             </form>
         </div>
     );
