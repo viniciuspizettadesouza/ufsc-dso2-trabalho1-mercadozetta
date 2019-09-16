a8d48ee36d203108118b758e5e463ac3719d9dd9
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 18:07:10 -0300

    new register function

--- backend/src/controller/userController.js
+++ backend/src/controller/userController.js
@@ -1,17 +1,17 @@
 const User = require('../model/User');
 
 module.exports = {
-    login(req, res) {
-        let { email, password } = req.body;
+    async register(req, res) {
+        const { email } = req.body;
 
-        User.findOne({ email: email, password: password }, function (err, email) {
-            if (err) {
-                return res.status(500).send();
-            }
-            if (!email) {
-                return res.status(404).send({ error: 'User not found' });
-            }
-            return res.status(200).send(req.body.email);
-        })
-    },
+        try {
+            if (await User.findOne({ email }))
+                return res.status(400).send({ error: 'User already exists' });
+            const newUser = await User.create(req.body);
+            newUser.password = undefined;
+            return res.status(200).send({ newUser });
+        } catch (err) {
+            return res.status(400).send({ error: 'Registration failed' });
+        }
+    }
 };
