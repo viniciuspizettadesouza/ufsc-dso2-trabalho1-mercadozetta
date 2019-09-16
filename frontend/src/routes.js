2e57a5f3f29e34ad49819817dd6b119561f72927
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-16 14:39:42 -0300

    new routes to RegisterProduct and RegisterUser

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
-import Register from './pages/Register';
+import RegisterProduct from './pages/RegisterProduct';
+import RegisterUser from './pages/RegisterUser';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Index from './pages/Index';
@@ -12,7 +13,8 @@ export default function Routes() {
         <BrowserRouter>
             <Route path="/" exact component={Index} />
             <Route path="/login" exact component={Login} />
-            <Route path="/register" component={Register} />
+            <Route path="/register/user" component={RegisterUser} />
+            <Route path="/register/product" component={RegisterProduct} />
             <Route path="/user/:id" component={Main} />
         </BrowserRouter>
     );
