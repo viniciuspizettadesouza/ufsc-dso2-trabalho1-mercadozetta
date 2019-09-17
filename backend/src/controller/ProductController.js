c1dc724ce45821211466c49823baa48949d881cd
vinicius pizetta de souza <viniciuspzt@gmail.com>
2019-09-17 02:03:24 -0300

    limpos comentários e logs

--- backend/src/controller/ProductController.js
+++ backend/src/controller/ProductController.js
@@ -1,18 +1,13 @@
 const Product = require('../model/Product');
 
 module.exports = {
-    // index de pesquisa de produtos ta bugado
-    // está retornando o objeto todo
-    // falta filtrar só os produtos do objeto user
 
     async index(req, res) {
 
         const products = await Product.find({}, function (err, name) {
             return (null, name)
         });
-        console.log(products);
         return res.status(200).send(products);
-
     },
 
     async add(req, res) {
